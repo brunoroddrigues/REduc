a5a0e395550e02cd9cc2d17e672a2d48aea3b503
DEREK ANIBAL NUNES <0200832211014>
2023-09-26 16:14:15 -0300

    Conexão feita e testada, dando inicio a função de cadastro

---
+++ Back-end/functions/Cadastro.php
@@ -0,0 +1,23 @@
+<?php
+
+// $categoria = $_POST['categoria'];
+
+echo '<pre>';
+var_dump($_POST);
+echo '<pre>';
+
+// function Cadastro(){
+//     $nome = $_POST['nome'];
+//     $sobrenome = $_POST['sobrenome'];
+//     $nomeUsuario = $_POST['username'];
+//     $cpf = $_POST['cpf'];
+//     $email = $_POST['email'];
+//     $senha = $_POST['senha1'];
+//     $pergunta = $_POST['pergunta'];
+//     $resposta = $_POST['resp'];
+//     $instituição = $_POST['instituicao'];
+//     if ($_POST['categoria']) {
+//         $categoria = 2;
+//     }
+    
+// }
